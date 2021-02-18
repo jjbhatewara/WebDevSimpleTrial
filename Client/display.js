fb871e0f6c8a4f7033fe030c723e8afcfb357af4
Jinesh Bhatewara <jinesh.21810002@viit.ac.in>
2021-02-18 16:09:20 +0530

    Display Done correct

--- Client/display.js
+++ Client/display.js
@@ -4,24 +4,24 @@
 //console.log(temp.data);
 document.addEventListener('DOMContentLoaded', ()=> {
     console.log("OKAY Dom is working");
-    loadHTMLTable();
-    fetch('http://localhost:5000/getData')
-    .then(console.log(rows)); 
+    fetch('http://localhost:5000/getData');
+/*     .then(response => response.json())
+    .then(data => loadHTMLTable(data['data'])) */
 });
 
 
 
-function loadHTMLTable(){
+function loadHTMLTable(data){
     const table = document.querySelector('table tbody');
-    table.innerHTML = "<tr>\
+/*     table.innerHTML = "<tr>\
     <th>1</th> \
 <th>Jinesh</th> \
 <th>06</th> \
 <th>COMP</th> \
-</tr>";
-    /* if(data.length == 0){
+</tr>"; */
+     if(data.length == 0){
         table.innerHTML = "<tr><td class = 'no-data' colspan = 5>No Data</td></tr>";    
 
-    } */
+    } 
 
 }
--- server/app.js
+++ server/app.js
@@ -16,6 +16,25 @@ const data = {
   headers:['ID','Name','Roll','branch'],
   rows : {}
 };
+
+function getAllData(){
+  try {
+    const response =  new Promise((resolve,reject) =>{
+        const query = 'SELECT * FROM `basic_name_roll` ';
+        connection.query(query,(err,results) =>{
+            if(err) reject(new Error(err.message));
+            resolve(results);
+        })
+    });
+    console.log(response);
+    return response;
+} catch (error) {
+    console.log(error);
+}
+}
+
+
+
 //const dataOfName = require('../Client/index');
 
 //console.log(dataOfName.)
@@ -34,18 +53,25 @@ app.post('/insert/:name/:roll/:branch',(req,res,next) => {
 
 
 app.get('/getData',(request,response)=>{
+    /* const result = getAllData();
+    result.then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+    console.log(result);
+ */
+
+
+
   //console.log("hello");
-  var getQuery = 'SELECT * FROM `basic_name_roll` ';
+   var getQuery = 'SELECT * FROM `basic_name_roll` ';
   connection.query(getQuery,(err,rows,fields)=>{
     if(!err){
       data.rows = rows;
-      response.json(rows);
-      //response.render('')
+      //response.json(rows);
+      response.render('C:/Users/Aditya/Desktop/TY/Other/Project managment learning/12-2-21 simple databasee/Client/EJS.ejs',{ rows : rows } );
       console.log("Displayed");
-      //console.log(rows);
-      //response.render('Students',{title:'Student details', items: rows})
+      console.log(rows);
     } 
-  });
+  }); 
 });
 
 app.listen("5000", ()=> console.log("app is running on PORT 5000"));
